c6f97aef9b8a4aab88c47521b04e2eef78a83e42
manhbv <manhbv001@gmail.com>
2025-07-27 22:38:20 +0700

    test

--- zod/postgresql/example.js
+++
@@ -1,9 +0,0 @@
-import { generateToJsonSchema } from '../../libs/generate-to-json-schema';
-import { z } from '../../libs/zod';
-
-generateToJsonSchema(
-  z.object({
-    id: z.string().max(125),
-    name: z.string().max(255),
-  }),
-);
